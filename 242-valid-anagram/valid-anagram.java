84c8e73213de584785daa308857734e17303ee68
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-04-18 21:57:07 -0400

    Time: 2 ms (98.34%) | Memory: 43.6 MB (62.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,44 @@
 class Solution {
+    // //Method 1: Using Arrays.
+    // public boolean isAnagram(String s, String t) {
+
+    //     //If the lenght doesn't match, the strings can not be anagrams.
+    //     if(s.length() != t.length()) return false;
+
+    //     // This is simpler method to use the Arrays library to sort and compare the two strings.
+    //     char[] s1 = s.toCharArray();
+    //     char[] t1 = t.toCharArray();
+
+    //     Arrays.sort(s1);
+    //     Arrays.sort(t1);
+
+    //     if(Arrays.equals(s1, t1)) return true;
+
+
+    //     return false;
+        
+    // }
+
+    //This problem uses the method of setting a 26 size array representing all alphabets. Since both the strings contains only lowercase english letters. Storing the freq of each char from the string in an int[] then comparing both the int[] to see if the words are anagrams of each other.
     public boolean isAnagram(String s, String t) {
 
         //If the lenght doesn't match, the strings can not be anagrams.
         if(s.length() != t.length()) return false;
 
-        char[] s1 = s.toCharArray();
-        char[] t1 = t.toCharArray();
 
-        Arrays.sort(s1);
-        Arrays.sort(t1);
-
-        if(Arrays.equals(s1, t1)) return true;
-
+        if(Arrays.equals(getFreq(s), getFreq(t))) return true;
 
         return false;
-        
     }
+
+    int[] getFreq(String s){
+
+        int[] count = new int[26];
+
+        for(char c : s.toCharArray())
+            count[c - 'a']++;
+
+        return count;
+    }
+
 }
