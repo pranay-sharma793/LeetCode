2501b43e0761cdeaba515cba6768ed4da6b13489
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-07-25 17:43:23 -0700

    Time: 23 ms (7.02%) | Memory: 48.7 MB (37.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,4 +1,13 @@
 class Solution {
+
+    /* 
+    Easiest approach to this is sorting the map values using collections, then creating a sublist of k elements. Picking the element key from the map with value from the topK list.
+
+    Learnings: Extracting map values in an list, collections sort on a list and reverse, creating subList, 
+    using list contains.
+    */
+
+
     public int[] topKFrequent(int[] nums, int k) {
 
         Map<Integer, Integer> map = new HashMap<>();
