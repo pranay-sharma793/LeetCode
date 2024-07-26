f504204f7d34db495b59867f1f0fc598e7c1faa7
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-07-25 20:57:20 -0700

    Time: 2 ms (86.31%) | Memory: 55.4 MB (45.52%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -10,28 +10,42 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
 
         int n = nums.length;
-        int[] lProd = new int[nums.length];
-        int[] rProd = new int[nums.length];
-        lProd[0] = rProd[n-1] = 1;
-        
-        int hasZeros = 0;
-
-        if(nums[0] == 0 || nums[n-1] == 0)
-            hasZeros++;
-
-        for(int i=1; i < nums.length; i++){
-
-            if(nums[i] == 0)
-                hasZeros++;
-
-            lProd[i] = nums[i-1]*lProd[i-1];
-            rProd[n-i-1] = nums[n-i]*rProd[n-i];
-        } 
+        // int[] lProd = new int[nums.length];
+        // int[] rProd = new int[nums.length];
+        // lProd[0] = rProd[n-1] = 1;
 
         int[] ans = new int[n];
-        for(int i =0; i < nums.length; i++){
-            ans[i] = lProd[i]*rProd[i];
+        ans[0] = 1;
+        int post = 1;
+
+
+        for(int i=1; i < n; i++){
+            ans[i] = nums[i-1]*ans[i-1];
         }
+
+        for(int i=n-2; i >= 0; i--){
+            post = post*nums[i+1];
+            ans[i] = post*ans[i];
+        }
+
+        
+        // int hasZeros = 0;
+
+        // if(nums[0] == 0 || nums[n-1] == 0)
+        //     hasZeros++;
+
+        // for(int i=1; i < nums.length; i++){
+
+        //     if(nums[i] == 0)
+        //         hasZeros++;
+
+        //     lProd[i] = nums[i-1]*lProd[i-1];
+        //     rProd[n-i-1] = nums[n-i]*rProd[n-i];
+        // } 
+
+        // for(int i =0; i < nums.length; i++){
+        //     ans[i] = lProd[i]*rProd[i];
+        // }
 
         return ans;
     }
