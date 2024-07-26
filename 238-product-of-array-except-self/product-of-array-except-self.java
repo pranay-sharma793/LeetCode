64b0ac1dfc9d1cb4cad2f8512e506c2edb662d75
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-07-25 19:11:47 -0700

    Time: 4 ms (8.04%) | Memory: 56.6 MB (5.71%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    /*
+    Trick: check zeros, if yes everything else 0.
+    left multiple and right multiple and ans is the final sum
+
+
+    */
+
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] lProd = new int[nums.length];
+        int[] rProd = new int[nums.length];
+        lProd[0] = rProd[n-1] = 1;
+        
+        int hasZeros = 0;
+
+        if(nums[0] == 0 || nums[n-1] == 0)
+            hasZeros++;
+
+        for(int i=1; i < nums.length; i++){
+
+            if(nums[i] == 0)
+                hasZeros++;
+
+            lProd[i] = nums[i-1]*lProd[i-1];
+            rProd[n-i-1] = nums[n-i]*rProd[n-i];
+        }
+
+        // if(hasZero == 2){
+        //     return new int[n];
+        // } else if(hasZero == 1){
+            
+        // }
+
+        int[] ans = new int[n];
+        for(int i =0; i < nums.length; i++){
+            ans[i] = lProd[i]*rProd[i];
+        }
+
+        return ans;
+    }
+}
