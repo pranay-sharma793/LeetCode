c8d1f662ce487cf2dafb6ac61c25a19e61d725f1
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-07-25 22:07:22 -0700

    Time: 22 ms (5.39%) | Memory: 45.9 MB (6.13%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        HashSet<String> rows = new HashSet<>();
+        HashSet<String> cols = new HashSet<>();
+        HashSet<String> sqs = new HashSet<>();
+
+        for(int r =0; r<9; r++){
+            for(int c=0; c<9; c++){
+
+                if(board[r][c] == '.')
+                    continue;
+
+                //check if already seen in row, col or square
+                if(rows.contains("r"+r+board[r][c]) || 
+                cols.contains("c"+c+board[r][c]) || 
+                sqs.contains("s"+r/3+c/3+board[r][c])){
+                    // System.out.println("diff at :: "+r + c+ (r/3+c/3));
+                    return false;
+                }
+
+                //adding visited elements in the hashsets created
+                //adding to row with row number and value
+                rows.add("r"+r+board[r][c]);
+                cols.add("c"+c+board[r][c]);
+                sqs.add("s"+r/3+c/3+board[r][c]);
+                // System.out.println("added :: " + "s"+ r/3+c/3+board[r][c] + " to sqs");
+            }
+        }
+
+        System.out.println(rows);
+        System.out.println(cols);
+        System.out.println(sqs);
+        
+        return true;
+    }
+}
