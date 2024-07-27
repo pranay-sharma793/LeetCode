f4262a16e12cfe6e20ca4a0558697d0c75ad2323
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-07-26 21:52:58 -0700

    Time: 17 ms (10.24%) | Memory: 45.8 MB (6.16%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -11,8 +11,8 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
 
         HashSet<String> rows = new HashSet<>();
-        HashSet<String> cols = new HashSet<>();
-        HashSet<String> sqs = new HashSet<>();
+        // HashSet<String> cols = new HashSet<>();
+        // HashSet<String> sqs = new HashSet<>();
 
         for(int r =0; r<9; r++){
             for(int c=0; c<9; c++){
@@ -22,21 +22,21 @@ class Solution {
 
                 //check if already seen in row, col or square
                 if(rows.contains("r"+r+board[r][c]) || 
-                cols.contains("c"+c+board[r][c]) || 
-                sqs.contains("s"+r/3+c/3+board[r][c])){
+                rows.contains("c"+c+board[r][c]) || 
+                rows.contains("s"+r/3+c/3+board[r][c])){
                     // System.out.println("diff at :: "+r + c+ (r/3+c/3));
                     return false;
                 }
 
                 //adding visited elements in the hashsets created
                 //adding to row with row number and value
-                rows.add("r"+r+board[r][c]);
-                cols.add("c"+c+board[r][c]);
-                sqs.add("s"+r/3+c/3+board[r][c]);
-
                 // rows.add("r"+r+board[r][c]);
-                // rows.add("c"+c+board[r][c]);
-                // rows.add("s"+r/3+c/3+board[r][c]);
+                // cols.add("c"+c+board[r][c]);
+                // sqs.add("s"+r/3+c/3+board[r][c]);
+
+                rows.add("r"+r+board[r][c]);
+                rows.add("c"+c+board[r][c]);
+                rows.add("s"+r/3+c/3+board[r][c]);
                 
             }
         }
