6894557ea804165eb374e3906acbf0ccabe46e82
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-07-26 22:44:24 -0700

    Time: 23 ms (78.85%) | Memory: 56.1 MB (96.39%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,3 +1,15 @@
+/*
+Trick to conver the num array to a set so that we can easily check.
+
+We here check if it a start of the sequence by checking n-1 and loop over performing n+1 until we do not have the next value. We then store the max value all the sequences. 
+
+Update: checking if have found a sequence more than half the length of the array. This basically menas we have the longest possible sequence. Here we break and return value.
+
+
+Learning: checking maxLen greater than half, putting array in a set. 
+ */
+
+
 class Solution {
     public int longestConsecutive(int[] nums) {
 
@@ -20,8 +32,10 @@ class Solution {
                     n++;
                 }
             }
-
             ans = Math.max(ans, count);
+
+
+            if(ans > nums.length/2) break;
         }
 
         return ans;
