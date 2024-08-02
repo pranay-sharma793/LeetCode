f471fdb756c44526053b6d07f364faa0f27f114e
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-08-02 00:50:43 -0400

    Time: 3 ms (73.13%) | Memory: 45.4 MB (16.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,22 +1,33 @@
+/**
+Trick: Storing each char which is a alphanumeric value in the char[]. Traverse the array half and compare with the other half. If value mismatch return false else return true.
+
+Alternative: Using a boolean flag.
+
+Learnings: toLowerCase, a - 97
+z - 122
+A - 67
+Z - 93
+0 - 48
+9 - 57
+ */
+
+
 class Solution {
     public boolean isPalindrome(String s) {
 
-        s = s.toLowerCase();
+        String str = s.toLowerCase();
+        List<Character> list = new ArrayList<>();
 
-        List<Character> str = new ArrayList<>();
-
-        for(char c : s.toCharArray()){
-            if((c >= 97 && c <= 122) || (c >= 48 && c <=57))
-                str.add(c);
+        for(char c: str.toCharArray()){
+            if((c >=97 && c <= 122) || (c >= 48 && c <= 57))
+                list.add(c);
         }
-        
-        int n = str.size();
-        for(int i=0; i < n/2; i++){
-            if(str.get(i) != str.get(n - i - 1))
+        int n = list.size();
+        for(int i =0; i < n/2; i++){
+            if(list.get(i) != list.get(n - i -1))
                 return false;
         }
-
-        return true;
         
+        return true;
     }
 }
