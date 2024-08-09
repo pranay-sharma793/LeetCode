64bc099e8c1e6a051104471e3a152602e186eb2d
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-08-09 17:58:36 -0400

    Time: 7 ms (82.16%) | Memory: 42.3 MB (84.59%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -3,25 +3,43 @@ class Solution {
 
         int maxChar = 0;
         int maxStr = 0;
-        int l=0, r=0;
-        Map<Character, Integer> map = new HashMap<>();
+        int l=0;
 
-        for(char c: s.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0)+1);
-            maxChar = Math.max(maxChar, map.get(c));
-            
-            if((r-l+1) - maxChar <= k){
-                maxStr = Math.max(maxStr, (r-l+1));
-                r++;
-            } else {
-                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+        //using the for loop as the right marker
+        //int r=0;
+
+        //Better to use a char counter array than a hashmap.
+        //Map<Character, Integer> map = new HashMap<>();
+
+        int[] charCount = new int[26];
+
+        for(int r=0; r<s.length(); r++){
+            charCount[s.charAt(r) - 'A']++;
+            maxChar = Math.max(maxChar, charCount[s.charAt(r) - 'A']);
+
+            while(r-l+1 - maxChar > k){
+                charCount[s.charAt(l) - 'A']--;
                 l++;
-                r++;
             }
 
-
+            maxStr = Math.max(maxStr , r-l+1);
 
         }
+
+
+        // for(char c: s.toCharArray()){
+        //     map.put(c, map.getOrDefault(c, 0)+1);
+        //     maxChar = Math.max(maxChar, map.get(c));
+            
+        //     if((r-l+1) - maxChar <= k){
+        //         maxStr = Math.max(maxStr, (r-l+1));
+        //         r++;
+        //     } else {
+        //         map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+        //         l++;
+        //         r++;
+        //     }
+        // }
         
         return maxStr;
     }
