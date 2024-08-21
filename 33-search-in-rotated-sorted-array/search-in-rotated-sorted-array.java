d0bcf79bff2ee9e72fb78addd2f60b4ecf38f409
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-08-20 20:26:57 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (17.90%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int start = 0;
+        int n = nums.length;
+        int end = n-1;
+
+        int ans = -1;
+
+        if(n == 1){
+            if(nums[0] == target) return 0;
+            else return -1;
+        }
+
+        if(nums[0] < nums[end]){
+            ans = perBS(nums, 0, n - 1,target);
+            return ans;
+        }
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int prev = (mid + n - 1) % n;
+            int next = (mid + 1) % n;
+
+            if(nums[mid] < nums[prev] && nums[mid] < nums[next]){
+                int tmp = nums[mid];
+                if(tmp == target) return mid;
+
+                if(nums[n-1] < target) ans = perBS(nums, 0, mid - 1,target);
+                else ans = perBS(nums, mid + 1, n-1,target);
+                break;
+
+            } else if(nums[0] <= nums[mid]) start = mid + 1;
+            else end = mid - 1;
+        }
+
+        return ans;
+        
+    }
+
+    public int perBS(int[] nums, int s, int e, int target){
+        int start = s;
+        int end = e;
+
+        while(start <= end){
+
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) end = mid -1;
+            else start = mid + 1;
+        }
+
+        return -1;
+    }
+}
