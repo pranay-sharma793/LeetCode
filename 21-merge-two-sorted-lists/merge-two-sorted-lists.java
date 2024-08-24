6a6dfd9bb6f8eabc4695fba0a917d8f95972298b
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-08-24 03:51:35 -0400

    Time: 0 ms (100.00%) | Memory: 42.3 MB (75.12%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -16,8 +16,6 @@ class Solution {
 
         while(list1 != null && list2 != null){
 
-            System.out.println(list1.val + " " + list2.val);
-
             if(list1.val < list2.val){
                 curr.next = list1;
                 list1 = list1.next;
