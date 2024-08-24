dc9f7868403cf138e4a05fc98a3888f58c582abc
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-08-24 00:15:09 -0400

    Time: 0 ms (100.00%) | Memory: 41.7 MB (7.12%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,4 +1,17 @@
 /**
+
+Loop over left to get to the position for curr to start reversing from the Next connections. hence left > 1.
+Store the connection node ( the node right before the reversal begin ) and the tail node ( the starting node from where the reversal is starting).
+
+While right > 0. Only reverse till right and perform simple reversal.
+
+Then if connection is not null (meaning reverse is after head) set connectio.next to prev (prev will be at the end of the reversal pointing the very last reversed). ELSE update head to prev as a normal LL reversal.
+
+And then for the remaining LL connect tail.next to curr which will be at the node right after the reversal stopped as a part of our increment.
+
+ */
+
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
