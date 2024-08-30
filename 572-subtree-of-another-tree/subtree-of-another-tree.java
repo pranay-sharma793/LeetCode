3491d2c828552cf0c897901987b0d9444559335a
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-08-29 22:54:23 -0400

    Time: 2 ms (97.21%) | Memory: 44.3 MB (54.33%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -1,4 +1,10 @@
 /**
+
+Time: O(n * m) where n is size of root tree and m is size of subtree.
+Space: O(h_n + h_m) height of the root tree
+ */
+
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -16,13 +22,14 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
 
+        // if we reach the end of the tree return false
         if(root == null) return false;
 
-        if(subRoot == null) return true;
+        //if we find the subtree in the main tree starting at position root, we return true
+        // if(isSameTree(root, subRoot)) return true;
 
-        if(isSameTree(root, subRoot)) return true;
-
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        // recursive call to find the subtree in left or right end. (we either find it and return true from a side or if both end and return false we return false)
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot) || isSameTree(root, subRoot);
 
     }
 
