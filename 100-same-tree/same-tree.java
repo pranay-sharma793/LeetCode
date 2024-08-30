df0c3a029b1818ea80dac6fd796d91327990cc34
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-08-30 03:54:13 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (27.94%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,4 +1,11 @@
 /**
+check if both null (T), check if one is null (F), check vals else return balance(left) && balance(right)
+
+Time: O(n + m) //size of both the trees. We can say it to be O(n), where n is the longer tree. 
+Space: O(height_n + height_m) //can be just O(height) which is the height of the longest tree.
+*/
+
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -16,12 +23,14 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
+        //checking if the structure is same
         if(p == null && q == null) return true;
-
         if((p == null && q != null) || (p != null && q == null)) return false;
 
+        // checking if the value are the same
         if(p.val != q.val) return false;
 
+        // call recursively to the right and left
         else return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         
     }
