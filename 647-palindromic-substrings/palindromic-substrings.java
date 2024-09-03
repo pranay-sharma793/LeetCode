f098153d9371c381b1c1b6719b05b9510273aaa8
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 17:31:30 -0400

    Time: 6 ms (74.63%) | Memory: 41.1 MB (95.34%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -2,7 +2,7 @@ class Solution {
     public int countSubstrings(String s) {
 
         if(s.length() <= 1) return 1;
-        
+
         int count = 0;
 
         for(int i = 0; i < s.length(); i++){
@@ -20,16 +20,12 @@ class Solution {
         int counter = 0;
 
         while(l >= 0 && r <= s.length()-1 && s.charAt(l) == s.charAt(r)){
-            // if(r - l + 1 > res.length()){
-            //     res = s.substring(l, r+1);
-            // }
             counter++;
             l-=1;
             r+=1;
 
         }
 
-        System.out.println(counter);
         return counter;
     }
 }
