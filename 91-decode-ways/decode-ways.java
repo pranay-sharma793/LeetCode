31e302fee8d97ce3dec86542b0e32bbd518b35a1
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 18:54:19 -0400

    Time: 1 ms (79.72%) | Memory: 41.9 MB (29.53%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,22 +1,22 @@
 class Solution {
     public int numDecodings(String s) {
 
-        int[] dp = new int[s.length() + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) == '0'? 0 : 1;
+        // int[] dp = new int[s.length() + 1];
+        // dp[0] = 1;
+        // dp[1] = s.charAt(0) == '0'? 0 : 1;
 
         int back = 1;
         int prev = s.charAt(0) == '0'? 0 : 1;
         int curr;
 
-        for(int i = 2; i < dp.length; i++){
+        for(int i = 2; i <= s.length(); i++){
 
             curr = 0;
 
             int val = Integer.parseInt(s.substring(i-1, i));
 
             if(val >= 1 && val <=9){
-                dp[i] += dp[i-1];
+                // dp[i] += dp[i-1];
 
                 curr += prev;
             }
@@ -24,7 +24,7 @@ class Solution {
             int valTwo = Integer.parseInt(s.substring(i-2, i));
 
             if(valTwo >= 10 && valTwo <= 26){
-                    dp[i] += dp[i-2];
+                    // dp[i] += dp[i-2];
 
                     curr += back;
             }
