526c3614d0b2526f321f3eda16f0577fbcb5015c
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 06:04:55 -0400

    Time: 9 ms (24.87%) | Memory: 40.5 MB (27.56%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    // Iterarive approach
+    public int fib(int n) {
+
+        if(n == 0) return 0;
+
+        if(n == 1) return 1;
+
+        return fib(n-1) + fib(n-2);
+        
+    }
+
+    // top down
+    public int fib1(int n) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 0);
+        map.put(1, 1);
+
+        return helper(map, n);
+        
+    }
+
+    private int helper(Map<Integer, Integer> map, int n){
+
+        if(map.containsKey(n)) return map.get(n);
+        else {
+            int prev1 = helper(map, n-1);
+            int prev2 = helper(map, n-2);
+            map.put(n, prev1 + prev2);
+        }
+
+        return map.get(n);
+    }
+}
