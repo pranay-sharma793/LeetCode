09b421deeb8923d9457eb4ad9339c3e1f624fc58
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 01:01:42 -0400

    Time: 0 ms (100.00%) | Memory: 44.2 MB (51.62%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -24,10 +24,16 @@ class Solution {
     private int helper(TreeNode root){
         if(root == null) return 0;
 
+        // This returns the max positive value left subtree can provide
         int left = Math.max(0, helper(root.left));
+
+        //This return the max positive value the right subtree can provide
         int right = Math.max(0, helper(root.right));
 
+        //This sets maxsum if the root, left and right itself have a bigger sum
         maxSum = Math.max(maxSum, left + right + root.val);
+
+        //here we return the value root + the biggest subtree value either on its left or right.
         return Math.max(left, right) + root.val;
 
 
