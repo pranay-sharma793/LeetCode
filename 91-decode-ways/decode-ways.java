fe5093d5cce7030e0ff3470c1dc7d524cdb3e2de
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 18:53:19 -0400

    Time: 1 ms (79.72%) | Memory: 42 MB (29.53%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -4,22 +4,37 @@ class Solution {
         int[] dp = new int[s.length() + 1];
         dp[0] = 1;
         dp[1] = s.charAt(0) == '0'? 0 : 1;
-        String tmp = "0123456";
+
+        int back = 1;
+        int prev = s.charAt(0) == '0'? 0 : 1;
+        int curr;
 
         for(int i = 2; i < dp.length; i++){
+
+            curr = 0;
+
             int val = Integer.parseInt(s.substring(i-1, i));
 
             if(val >= 1 && val <=9){
                 dp[i] += dp[i-1];
+
+                curr += prev;
             }
 
             int valTwo = Integer.parseInt(s.substring(i-2, i));
 
             if(valTwo >= 10 && valTwo <= 26){
                     dp[i] += dp[i-2];
+
+                    curr += back;
             }
+
+            int tmp = prev;
+            prev = curr;
+            back = tmp;
         }
         
-        return dp[s.length()];
+        return prev;
+        // return dp[s.length()];
     }
 }
