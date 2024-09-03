6fa5792bc5c0282ba77bacd5518eaaaa66f96338
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 00:35:03 -0400

    Time: 1 ms (99.78%) | Memory: 44.3 MB (76.60%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -18,26 +18,26 @@ class Solution {
     List<Integer> res = new ArrayList<>();
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
 
-        helper(root, targetSum, 0);
+        helper(root, targetSum);
 
         return result;
     }
 
-    private void helper(TreeNode root, int target, int curr_sum){
+    private void helper(TreeNode root, int target){
         if(root == null) return;
         
         res.add(root.val);
-        curr_sum += root.val;
+        // curr_sum += root.val;
 
-        if(root.left == null && root.right == null){
-            if(curr_sum == target){
+        if(root.left == null && root.right == null && target == root.val){
                 result.add(new ArrayList<>(res));
-                // return;
-            } 
         } 
 
-        helper(root.left, target, curr_sum);
-        helper(root.right, target, curr_sum);
+        // helper(root.left, target, curr_sum);
+        // helper(root.right, target, curr_sum);
+
+        helper(root.left, target - root.val);
+        helper(root.right, target - root.val);
 
         int s = res.size();
         res.remove(s-1);
