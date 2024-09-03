b14d7666abadaf8ec64827eeacf951e25d68fea4
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 17:36:19 -0400

    Time: 5 ms (96.07%) | Memory: 41.3 MB (78.13%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,3 +1,11 @@
+/**
+
+Code similar to longest palindrome used here. For evert char match during center expansion we increase a counter. We sum all the counter returns. (There are basically all the palindrome possible from that letter (i)) we do it for odd and even centers and sum it all togther and return the final value.
+
+Time: O(n^2)
+Space: O(1)
+ */
+
 class Solution {
     public int countSubstrings(String s) {
 
@@ -23,7 +31,6 @@ class Solution {
             counter++;
             l-=1;
             r+=1;
-
         }
 
         return counter;
