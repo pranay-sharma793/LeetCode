6ee10f298482acebf97b2ce80ce9e843921e86d7
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-04 05:23:06 -0400

    Time: 14 ms (88.46%) | Memory: 44.1 MB (75.12%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,33 @@
+/**
+Bottom up approach to calculate for amount using a dp array.
+
+	•	Time Complexity: O(amount * n)
+	•	Space Complexity: O(amount)
+
+ */
+
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
 
-        int dp[] = new int[amount+1];
+        int dp[] = new int[amount + 1];
 
-        for(int i = 1; i < dp.length; i++)
+        for(int i = 0; i < dp.length; i++){
             dp[i] = amount+1;
+        }
 
-        // dp[0] = 0;
+        dp[0] = 0;
 
-        for(int i = 0; i <= amount; i++){
+        for(int i = 1; i<=amount; i++){
             for(int c : coins){
                 if(i - c >= 0){
-                    dp[i] = Math.min(dp[i], 1 + dp[i - c]);
+                    dp[i] = Math.min(dp[i], 1 + dp[i-c]);
                 }
             }
         }
 
-        return dp[amount] == amount + 1? -1:dp[amount];
+        return dp[amount] == amount+1? -1 : dp[amount];
+
         
     }
 }
