aad5afb1153dc99571296bd488874046481ee740
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-04 07:21:12 -0400

    Time: 1 ms (95.69%) | Memory: 44.8 MB (45.04%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,3 +1,14 @@
+/**
+
+Since nums also contain negaitve values. We have to optimise our approach.
+Here what we do is that take products of numbers from both the ends.
+At each stage we store the max prod.
+When we encounter a 0, we reset the pre and suff to 1.
+
+	•	Time Complexity: O(n)
+	•	Space Complexity: O(1)
+ */
+
 class Solution {
     public int maxProduct(int[] nums) {
 
@@ -7,7 +18,7 @@ class Solution {
         int maxProd = nums[0];
 
         for(int i = 0; i < nums.length; i++){
-            
+
             int preVal = nums[i];
             int sufVal =nums[nums.length - i -1];
 
