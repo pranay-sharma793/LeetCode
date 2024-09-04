4193649f5d62d41bc1212a5b8842127cb98d61c9
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 22:57:15 -0400

    Time: 1 ms (79.72%) | Memory: 41.6 MB (64.90%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,6 +1,36 @@
 class Solution {
     public int numDecodings(String s) {
 
+        int backTwice = 1;
+        int backOnce = s.charAt(0) == '0' ? 0:1;
+        int curr;
+
+        for(int i = 2; i <= s.length(); i++){
+
+            int singleVal = Integer.valueOf(s.substring(i-1, i));
+
+            curr = 0;
+
+            if(singleVal >= 1 && singleVal <= 9 ){
+                curr = backOnce;
+            }
+
+            int doubleVal = Integer.valueOf(s.substring(i-2, i));
+
+            if(doubleVal >= 10 && doubleVal <= 26){
+                curr += backTwice;
+            }
+
+            int tmp = backOnce;
+            backOnce = curr;
+            backTwice = tmp;
+        }
+
+        return backOnce;
+    }
+
+    public int numDecodings1(String s) {
+
         // int[] dp = new int[s.length() + 1];
         // dp[0] = 1;
         // dp[1] = s.charAt(0) == '0'? 0 : 1;
@@ -16,17 +46,17 @@ class Solution {
             int val = Integer.parseInt(s.substring(i-1, i));
 
             if(val >= 1 && val <=9){
-                // dp[i] += dp[i-1];
+                // dp[i] = dp[i-1];
 
-                curr += prev;
+                curr = prev;
             }
 
             int valTwo = Integer.parseInt(s.substring(i-2, i));
 
             if(valTwo >= 10 && valTwo <= 26){
-                    // dp[i] += dp[i-2];
+                // dp[i] += dp[i-2];
 
-                    curr += back;
+                curr += back;
             }
 
             int tmp = prev;
