8b8c6feb45d9d0a4d90c7d15997b104b9fb665a2
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 23:46:15 -0400

    Time: 16 ms (60.33%) | Memory: 44.6 MB (43.68%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,10 +3,10 @@ class Solution {
 
         int dp[] = new int[amount+1];
 
-        for(int i = 0; i < dp.length; i++)
+        for(int i = 1; i < dp.length; i++)
             dp[i] = amount+1;
 
-        dp[0] = 0;
+        // dp[0] = 0;
 
         for(int i = 0; i <= amount; i++){
             for(int c : coins){
