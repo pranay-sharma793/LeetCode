1d4758028bb9a7d4d8f01fc34ca37d8b822d9829
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-03 22:49:09 -0400

    Time: 0 ms (100.00%) | Memory: 40.1 MB (67.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,60 +1,19 @@
 class Solution {
-    // Iterarive approach
-    public int fib1(int n) {
-
-        if(n == 0) return 0;
-
-        if(n == 1) return 1;
-
-        return fib(n-1) + fib(n-2);
-        //TIme: O(2*n)
-        //Space: O(2n)
-    }
-
-    // top down
-    public int fib2(int n) {
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        map.put(0, 0);
-        map.put(1, 1);
-
-        return helper(map, n);
-        //TIme: O(n)
-        //Space: O(n)
-        
-    }
-
-    private int helper(Map<Integer, Integer> map, int n){
-
-        if(map.containsKey(n)) return map.get(n);
-        else {
-            int prev1 = helper(map, n-1);
-            int prev2 = helper(map, n-2);
-            map.put(n, prev1 + prev2);
-        }
-
-        return map.get(n);
-
-    }
-
-    // bottom up in constant time
     public int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
 
-        int curr = 1;
+        if(n <= 1) return n;
+
         int prev = 0;
+        int curr = 1;
 
         for(int i = 2; i <= n; i++){
+
             int tmp = curr;
             curr += prev;
             prev = tmp;
         }
 
         return curr;
-
-        //TIme: O(n)
-        //Space: O(1)
+        
     }
 }
