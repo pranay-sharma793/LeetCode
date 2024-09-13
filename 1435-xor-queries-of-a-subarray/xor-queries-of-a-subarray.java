ff8aa4abf932aa4d47652f6effb62a35d883770b
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-12 23:45:14 -0400

    Time: 2 ms (100.00%) | Memory: 57.6 MB (61.65%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,6 +1,14 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
 
+        //using prefix XOR array
+        int[] prefix = new int[arr.length];
+
+        prefix[0] = arr[0];
+        for(int i =1; i < arr.length; i++){
+            prefix[i] = prefix[i - 1] ^ arr[i];
+        }
+
         int[] res = new int[queries.length];
         int i =0;
 
@@ -8,9 +16,12 @@ class Solution {
             int left = query[0];
             int right = query[1];
 
-            for(int j=left; j <= right; j++){
-                res[i] ^= arr[j];
-            }
+            if(left == 0) res[i] = prefix[right];
+            else res[i] = prefix[right] ^ prefix[left -1];
+
+            // for(int j=left; j <= right; j++){
+            //     res[i] ^= arr[j];
+            // }
             i++;
 
         }
