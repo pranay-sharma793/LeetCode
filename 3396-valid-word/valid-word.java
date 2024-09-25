ea9205548dad40b74b6d68b1a57bc3ec5c5046b0
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-25 01:32:53 -0400

    Time: 1 ms (97.88%) | Memory: 42.2 MB (31.86%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -35,9 +35,9 @@ class Solution {
     }
 
     private boolean isVowel(char c){
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
-        // String vowels = "aeiouAEIOU";
-        // return vowels.indexOf(c)==-1?false:true;
+        // return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+        // c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+        String vowels = "aeiouAEIOU";
+        return vowels.indexOf(c)==-1?false:true;
     }
 }
