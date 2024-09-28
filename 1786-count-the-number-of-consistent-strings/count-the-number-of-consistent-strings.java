90920f524fff424a204c0fdfe7d8dc5aeeba99c2
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-27 23:19:14 -0400

    Time: 5 ms (89.43%) | Memory: 45.3 MB (39.07%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
@@ -8,15 +8,26 @@ class Solution {
             freq[c - 'a'] = true;;
         }
 
+        // for(String word : words){
+        //     int flag = 1;
+        //     for(int i=0; i < word.length(); i++){
+        //         if(i != 0 && word.charAt(i) == word.charAt(i-1)){
+        //             continue;
+        //         }
+        //         char c = word.charAt(i);
+
+        //         if(!freq[c - 'a']){
+        //             flag = 0;
+        //             break;
+        //         }
+        //     }
+        //     output += flag;
+        // }
+
         for(String word : words){
             int flag = 1;
             for(int i=0; i < word.length(); i++){
-                if(i != 0 && word.charAt(i) == word.charAt(i-1)){
-                    continue;
-                }
-                char c = word.charAt(i);
-
-                if(!freq[c - 'a']){
+                if(!freq[word.charAt(i) - 'a']){
                     flag = 0;
                     break;
                 }
