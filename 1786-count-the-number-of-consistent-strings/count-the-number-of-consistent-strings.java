5ccaf03f987b98f609e1d329f5b51c680042fe5d
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-09-27 23:00:35 -0400

    Time: 19 ms (14.92%) | Memory: 45.4 MB (39.07%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
@@ -1,41 +1,23 @@
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
 
-        int[] freq = new int[26];
-
-        for(int i =0; i< allowed.length();i++){
-            if(i !=0 && allowed.charAt(i)==allowed.charAt(i-1)){
-                continue;
-            } else {
-                freq[allowed.charAt(i) - 'a']++;
-            }
-        }
-
-        for(int i=0; i<26;i++){
-            System.out.print(freq[i]);
-        }
-
-        // Set<Character> set = new HashSet<>();
-
-        // for(char c : allowed.toCharArray()){
-        //     set.add(c);
-        // }
-        
-        int res = words.length;
+        int output = words.length;
 
         for(String word : words){
-            for(int i =0; i< word.length();i++){
-                if(i !=0 && word.charAt(i)==word.charAt(i-1)){
+            for(int i=0; i < word.length(); i++){
+                if(i != 0 && word.charAt(i) == word.charAt(i-1)){
                     continue;
                 }
-                if(freq[word.charAt(i) - 'a'] == 0){
-                    res--;
+                char c = word.charAt(i);
+
+                if(allowed.indexOf(c) == -1){
+                    output--;
                     break;
                 }
             }
         }
 
-
-        return res;
+        return output;
+        
     }
 }
