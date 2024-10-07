5a3804620807735b69e9d021d5f833d46b94ed80
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-10-07 06:56:38 -0400

    Time: 15 ms (38.15%) | Memory: 48.2 MB (76.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,39 +1,25 @@
 class Solution {
-
-    /* 
-    Easiest approach to this is sorting the map values using collections, then creating a sublist of k elements. Picking the element key from the map with value from the topK list.
-
-    Learnings: Extracting map values in an list, collections sort on a list and reverse, creating subList, 
-    using list contains.
-
-    Alternative solutions: priority queue, bucket sort, min/max heap
-    */
-
-
     public int[] topKFrequent(int[] nums, int k) {
 
         Map<Integer, Integer> map = new HashMap<>();
-        int[] ans = new int[k];
-        int b = 0;
 
-
-        for(int i : nums){
-            map.put(i , map.getOrDefault(i, 0) + 1);
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0) + 1);
         }
 
-        List<Integer> vals = new ArrayList<>(map.values());
-        Collections.sort(vals, Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
 
-        List<Integer> topK = vals.subList(0, Math.min(k, vals.size()));
-
-        for(Map.Entry<Integer, Integer> val: map.entrySet()){
-            if(topK.contains(val.getValue())){
-                ans[b] = val.getKey();
-                b++;
-            }
+        for(int key : map.keySet()){
+            pq.add(key);
         }
 
-        return ans;
+        int[] res = new int[k];
+
+        for(int i=0; i<k;i++){
+            res[i] = pq.poll();
+        }
+
+        return res;
         
     }
 }
