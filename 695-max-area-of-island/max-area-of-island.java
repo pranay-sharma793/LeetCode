3adc983e6e167588fc001a8b4cdae7f907625fc8
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-11-23 03:57:57 -0500

    Time: 2 ms (70.79%) | Memory: 44.3 MB (81.86%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,19 +1,19 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         
-        int m_area = 0;
+        int max_area = 0;
 
-        for(int i=0; i < grid.length; i++){
-            for(int j=0; j < grid[0].length; j++){
+        for(int i =0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
 
                 if(grid[i][j] == 1){
-                    int area = getArea(grid, i ,j);
-                    m_area = Math.max(m_area, area);
+                    int area = getArea(grid, i, j);
+                    max_area = Math.max(max_area, area);
                 }
             }
         }
 
-        return m_area;
+        return max_area;
     }
 
     private int getArea(int[][] grid, int i, int j){
@@ -25,16 +25,10 @@ class Solution {
         int area = 1;
 
         area += getArea(grid, i+1, j);
-        area+= getArea(grid, i-1, j);
+        area += getArea(grid, i-1, j);
         area += getArea(grid, i, j+1);
         area += getArea(grid, i, j-1);
 
         return area;
-
-        // if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != 1) return 0;
-
-
-
-        // return 1 + getArea(grid, i+1, j) + getArea(grid, i-1, j) + getArea(grid, i, j+1) + getArea(grid, i, j-1);
     }
 }
