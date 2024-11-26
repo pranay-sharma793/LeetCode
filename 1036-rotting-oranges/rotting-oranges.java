9b0cdd933e24abe0e5660ac85f6727944864538c
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-11-26 00:46:47 -0500

    Time: 3 ms (14.77%) | Memory: 42.8 MB (18.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,106 +1,63 @@
-/**
-Working solution but with out BFS.
-Have to retry this problem with queue and BFS approach. Keeping adding each updated 1 to 2 in the queue while queue is not empty. Keep a track of the initial 1s in the 0th run. If 1s > 0 after all return -1 else return min.
-BFS time will be O(m * n)
-
-
-Current soln time: O((n * m) * k) //where k is minutes
-Space: O(n * m) /worst case for list
- */
 class Solution {
     public int orangesRotting(int[][] grid) {
 
-        int min = 0;
-        List<int[]> rotPos = new ArrayList<>();
+        int freshOrgs = 0;
+        Queue<int[]> queue = new LinkedList<>();
 
-        rotPos = findRottenOrange(grid);
+        int[][] dist = {{1,0}, {-1,0}, {0,1}, {0, -1}};
 
-        while(rotPos.size() > 0 && updateNextMinute(rotPos, grid)){
-            rotPos = findRottenOrange(grid);
-            min++;
-        }
+        for(int r=0; r < grid.length; r++){
+            for(int c=0; c < grid[0].length; c++){
 
-        if(stillFresh(grid)) return -1;
-
-        return min;
-    }
-
-    private List<int[]> findRottenOrange(int[][] grid){
-
-        int[] pos;
-        List<int[]> result= new ArrayList<>();
-
-        for(int i=0; i<grid.length;i++){
-            for(int j =0; j < grid[i].length; j++){
-                if(grid[i][j] == 2){
-                    pos = new int[2];
-                    pos[0]=i;
-                    pos[1]=j;
-                    result.add(pos);
-            }
-        }
-      }
-
-      return result;
-
-    }
-
-    private boolean updateNextMinute(List<int[]> rotPos, int[][] grid){
-        boolean didUpdate = false;
-
-        for(int k=0; k < rotPos.size(); k++){
-            int[] pos = rotPos.get(k);
-            int i = pos[0];
-            int j = pos[1];
-
-            grid[i][j] = -1;
-            
-            if(i+1 >= 0 && i+1 < grid.length){
-                if(grid[i+1][j]==1){
-                    grid[i+1][j] = 2;
-                    didUpdate = true;
+                if(grid[r][c] == 2){
+                    queue.add(new int[] {r,c});
+                }
+                if(grid[r][c] == 1){
+                    freshOrgs++;
                 }
             }
-
-            if(i-1 >= 0 && i-1 < grid.length){
-                if(grid[i-1][j]==1){
-                    grid[i-1][j] = 2;
-                    didUpdate = true;
-                }
-            }
-
-            if(j+1 >= 0 && j+1 < grid[i].length){
-                if(grid[i][j+1]==1){
-                    grid[i][j+1] = 2;
-                    didUpdate = true;
-                }
-            }
-
-            if(j-1 >= 0 && j-1 < grid[i].length){
-                if(grid[i][j-1]==1){
-                    grid[i][j-1] = 2;
-                    didUpdate = true;
-                }
-            }
-            
         }
 
-        return didUpdate;
-    }
+        if(freshOrgs == 0) return 0;
 
-    private boolean stillFresh(int[][] grid){
+        int time = 0;
+        while(!queue.isEmpty()){
 
+            int size = queue.size();
+            time++;
 
-        for(int i=0; i<grid.length;i++){
-        for(int j =0; j < grid[i].length; j++){
-            if(grid[i][j] == 1){
-                return true;
+            for(int k=0; k < size; k++){
+
+                int[] pos = queue.poll();
+
+                for(int[] dir: dist){
+                    int new_r = pos[0] + dir[0];
+                    int new_c = pos[1] + dir[1];
+              
+                    if(rottenOrange(grid, new_r, new_c)){
+                        freshOrgs--;
+                        queue.add(new int[] {new_r, new_c});
+                    }
+                
+                }
+                if(freshOrgs == 0) return time;
+
             }
         }
-      }
 
-      return false;
-      
+        return freshOrgs==0?time:-1;
+        
     }
 
+    private boolean rottenOrange(int[][] grid, int r, int c){
+
+        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] != 1) return false;
+
+        if(grid[r][c] == 1){
+            grid[r][c] = 2;
+            return true;
+        }
+
+        return false;
+    }
 }
