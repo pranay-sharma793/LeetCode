e2b5ff015188c7f0c1d046b0ed5d42c065a0d621
Pranay Sharma <78352063+pranay-sharma793@users.noreply.github.com>
2024-12-01 17:36:35 -0500

    Time: 24 ms (25.31%) | Memory: 47.4 MB (82.68%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0], b[0]));
+
+        for(int[] time : times){
+            int from = time[0];
+            int to = time[1];
+            int weight = time[2];
+
+            List<int[]> tmp = map.getOrDefault(from, new ArrayList<>());
+            tmp.add(new int[] {to, weight});
+            map.put(from, tmp);
+        }
+
+        pq.offer(new int[] {0, k});
+        int res = 0;
+        Set<Integer> visited = new HashSet<>();
+
+        while(!pq.isEmpty()){
+            int[] node = pq.poll();
+            int w1 = node[0];
+            int n1 = node[1];
+
+            if(visited.contains(n1)) continue;
+
+            visited.add(n1);
+            res=w1;
+
+            if(map.containsKey(n1)){
+                for(int[] travel : map.get(n1)){
+                    if(!visited.contains(travel[0])){
+                        pq.offer(new int[] {w1+travel[1], travel[0]});
+                    }
+                }
+            }
+        }
+
+
+        return visited.size()==n?res:-1;
+    }
+}
